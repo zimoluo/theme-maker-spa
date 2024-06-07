113dba313811252cb26d2c3c76e35b9ee1f1caf1
zimoluo <zimo_luo@outlook.com>
2024-06-07 14:39:39 +0800

    real fullscreen lets go

--- app/design/theme-maker/ThemeMakerWindowWrapper.tsx
+++ app/design/theme-maker/ThemeMakerWindowWrapper.tsx
@@ -14,8 +14,8 @@ export default function ThemeMakerWindowWrapper({ children }: Props) {
 
   return (
     <div
-      className={`${windowStyle.transition} mt-16 ${
-        isFullscreen ? "md:mb-6" : "md:my-18 md:rounded-3xl md:shadow-xl"
+      className={`${windowStyle.transition} mt-12 ${
+        isFullscreen ? "" : "md:my-16 md:rounded-3xl md:shadow-xl"
       } ${
         isFullscreen ? windowStyle.fullscreen : windowStyle.sizing
       } bg-widget-80 md:bg-widget-40 md:backdrop-blur-xl md:overflow-hidden ${
