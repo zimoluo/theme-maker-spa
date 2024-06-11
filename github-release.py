3e31b03c934d7974536cbe40bd54c591ed4163bc
zimoluo <zimo_luo@outlook.com>
2024-06-11 15:48:36 +0800

    1.8.5

--- github-release.py
+++ github-release.py
@@ -17,7 +17,7 @@ def main():
     GITHUB_REPO = os.getenv('GITHUB_REPO')
     GITHUB_API_URL = f'https://api.github.com/repos/{GITHUB_REPO}/releases'
 
-    VERSION = '1.8.4'
+    VERSION = '1.8.5'
 
     # Determine the script to run based on the operating system
     if platform.system() == 'Windows':
