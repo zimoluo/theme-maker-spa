195ab2c5e742e551083f83d21d09b3d74ec2e1e3
zimoluo <zimo_luo@outlook.com>
2024-06-11 15:29:54 +0800

    remove the page effect stuff

--- components/mainPage/MainPageEffect.tsx
+++ components/mainPage/MainPageEffect.tsx
@@ -1,10 +1,7 @@
 "use client";
 
 import { ReactNode, useEffect } from "react";
-import { isBirthday, isChristmas, isHalloween } from "@/lib/seasonUtil";
 import { parseStoredSettings, useSettings } from "../contexts/SettingsContext";
-import { defaultSettings } from "@/lib/constants/defaultSettings";
-import _ from "lodash";
 import ToastBannerReceiver from "../widgets/ToastBannerReceiver";
 import ToastDisplayLegacy from "../widgets/ToastDisplayLegacy";
 
@@ -16,20 +13,6 @@ const toastComponentMap: Record<NotificationStyle, ReactNode> = {
   disabled: null,
   toast: <ToastDisplayLegacy />,
   banner: <ToastBannerReceiver />,
-};
-
-const pageKeys: NavigationKey[] = [
-  ...(Object.keys(defaultSettings.pageTheme) as NavigationKey[]),
-];
-
-const getUniformPageTheme = (
-  theme: ThemeKey
-): Record<NavigationKey, ThemeKey> => {
-  const pageTheme = pageKeys.reduce((themeObject, key) => {
-    (themeObject as Record<NavigationKey, ThemeKey>)[key] = theme as ThemeKey;
-    return themeObject;
-  }, {});
-  return pageTheme as Record<NavigationKey, ThemeKey>;
 };
 
 export default function MainPageEffect({ children }: Props) {
@@ -45,50 +28,7 @@ export default function MainPageEffect({ children }: Props) {
       return loadedSettings;
     }
 
-    downloadUserInfo().then((preparedSettings) => {
-      if (
-        _.isEqual(preparedSettings.pageTheme, defaultSettings.pageTheme) &&
-        window.matchMedia("(prefers-color-scheme: dark)").matches
-      ) {
-        updateSettings(
-          {
-            ...preparedSettings,
-            pageTheme: getUniformPageTheme("plainDark"),
-          },
-          false
-        );
-      }
-
-      if (isHalloween()) {
-        updateSettings(
-          {
-            ...preparedSettings,
-            pageTheme: getUniformPageTheme("halloween"),
-          },
-          false
-        );
-      }
-
-      if (isBirthday()) {
-        updateSettings(
-          {
-            ...preparedSettings,
-            pageTheme: getUniformPageTheme("birthday"),
-          },
-          false
-        );
-      }
-
-      if (isChristmas()) {
-        updateSettings(
-          {
-            ...preparedSettings,
-            pageTheme: getUniformPageTheme("christmas"),
-          },
-          false
-        );
-      }
-    });
+    downloadUserInfo().then((preparedSettings) => {});
   }, []);
 
   return (
