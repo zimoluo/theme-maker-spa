fd7f756c468beb3e3db4fb3ff1f998a926265bd0
zimoluo <zimo_luo@outlook.com>
2024-07-09 14:17:06 +0800

    fix more issues

--- components/theme/util/ThemeDataInitializer.tsx
+++ components/theme/util/ThemeDataInitializer.tsx
@@ -2,7 +2,7 @@
 
 import { ReactNode, useMemo } from "react";
 import { ThemeProvider } from "@/components/contexts/ThemeContext";
-import { useSettings } from "../contexts/SettingsContext";
+import { useSettings } from "../../contexts/SettingsContext";
 import { useNavigation } from "@/lib/helperHooks";
 
 interface Props {
