706762c334ed345e471707b94d873163a389d256
zimoluo <zimo_luo@outlook.com>
2024-09-12 19:15:26 -0400

    port changes

--- components/theme/util/backgroundAnimationMap.tsx
+++ components/theme/util/backgroundAnimationMap.tsx
@@ -1,27 +1,28 @@
 import { ReactNode } from "react";
-import PhotosAnimatedBackground from "../../mainPage/backgroundAnimations/photos/PhotosAnimatedBackground";
-import HomeAnimatedBackground from "../../mainPage/backgroundAnimations/home/HomeAnimatedBackground";
-import BlogAnimatedBackground from "../../mainPage/backgroundAnimations/blog/BlogAnimatedBackground";
-import ProjectsAnimatedBackground from "../../mainPage/backgroundAnimations/projects/ProjectsAnimatedBackground";
-import MidnightAnimatedBackground from "../../mainPage/backgroundAnimations/midnight/MidnightAnimatedBackground";
-import GlitterAnimatedBackground from "../../mainPage/backgroundAnimations/glitter/GlitterAnimatedBackground";
-import BirthdayAnimatedBackground from "../../mainPage/backgroundAnimations/birthday/BirthdayAnimatedBackground";
-import RainbowScroll from "../../mainPage/backgroundAnimations/rainbow/RainbowScroll";
-import BubblesAnimatedBackground from "../../mainPage/backgroundAnimations/bubbles/BubblesAnimatedBackground";
-import ShootingStars from "../../mainPage/backgroundAnimations/star/ShootingStars";
-import ChristmasAnimatedBackground from "../../mainPage/backgroundAnimations/christmas/ChristmasAnimatedBackground";
-import GrassAnimatedBackground from "../../mainPage/backgroundAnimations/grass/GrassAnimatedBackground";
-import HalloweenAnimatedBackground from "../../mainPage/backgroundAnimations/halloween/HalloweenAnimatedBackground";
-import AboutAnimatedBackground from "../../mainPage/backgroundAnimations/about/AboutAnimatedBackground";
-import GoldAnimatedBackground from "../../mainPage/backgroundAnimations/gold/GoldAnimatedBackground";
-import SkyAnimatedBackground from "../../mainPage/backgroundAnimations/sky/SkyAnimatedBackground";
-import StormAnimatedBackground from "../../mainPage/backgroundAnimations/storm/StormAnimatedBackground";
-import PixellandAnimatedBackground from "../../mainPage/backgroundAnimations/pixelland/PixellandAnimatedBackground";
-import VerdantAnimatedBackground from "../../mainPage/backgroundAnimations/verdant/VerdantAnimatedBackground";
-import BewitchedAnimatedBackground from "../../mainPage/backgroundAnimations/bewitched/BewitchedAnimatedBackground";
-import UnderwaterAnimatedBackground from "../../mainPage/backgroundAnimations/underwater/UnderwaterAnimatedBackground";
-import CrimsonAnimatedBackground from "../../mainPage/backgroundAnimations/crimson/CrimsonAnimatedBackground";
-import EepAnimatedBackground from "../../mainPage/backgroundAnimations/eep/EepAnimatedBackground";
+import PhotosAnimatedBackground from "@/components/mainPage/backgroundAnimations/photos/PhotosAnimatedBackground";
+import HomeAnimatedBackground from "@/components/mainPage/backgroundAnimations/home/HomeAnimatedBackground";
+import BlogAnimatedBackground from "@/components/mainPage/backgroundAnimations/blog/BlogAnimatedBackground";
+import ProjectsAnimatedBackground from "@/components/mainPage/backgroundAnimations/projects/ProjectsAnimatedBackground";
+import MidnightAnimatedBackground from "@/components/mainPage/backgroundAnimations/midnight/MidnightAnimatedBackground";
+import GlitterAnimatedBackground from "@/components/mainPage/backgroundAnimations/glitter/GlitterAnimatedBackground";
+import BirthdayAnimatedBackground from "@/components/mainPage/backgroundAnimations/birthday/BirthdayAnimatedBackground";
+import RainbowScroll from "@/components/mainPage/backgroundAnimations/rainbow/RainbowScroll";
+import BubblesAnimatedBackground from "@/components/mainPage/backgroundAnimations/bubbles/BubblesAnimatedBackground";
+import ShootingStars from "@/components/mainPage/backgroundAnimations/star/ShootingStars";
+import ChristmasAnimatedBackground from "@/components/mainPage/backgroundAnimations/christmas/ChristmasAnimatedBackground";
+import GrassAnimatedBackground from "@/components/mainPage/backgroundAnimations/grass/GrassAnimatedBackground";
+import HalloweenAnimatedBackground from "@/components/mainPage/backgroundAnimations/halloween/HalloweenAnimatedBackground";
+import AboutAnimatedBackground from "@/components/mainPage/backgroundAnimations/about/AboutAnimatedBackground";
+import GoldAnimatedBackground from "@/components/mainPage/backgroundAnimations/gold/GoldAnimatedBackground";
+import SkyAnimatedBackground from "@/components/mainPage/backgroundAnimations/sky/SkyAnimatedBackground";
+import StormAnimatedBackground from "@/components/mainPage/backgroundAnimations/storm/StormAnimatedBackground";
+import PixellandAnimatedBackground from "@/components/mainPage/backgroundAnimations/pixelland/PixellandAnimatedBackground";
+import VerdantAnimatedBackground from "@/components/mainPage/backgroundAnimations/verdant/VerdantAnimatedBackground";
+import BewitchedAnimatedBackground from "@/components/mainPage/backgroundAnimations/bewitched/BewitchedAnimatedBackground";
+import UnderwaterAnimatedBackground from "@/components/mainPage/backgroundAnimations/underwater/UnderwaterAnimatedBackground";
+import CrimsonAnimatedBackground from "@/components/mainPage/backgroundAnimations/crimson/CrimsonAnimatedBackground";
+import EepAnimatedBackground from "@/components/mainPage/backgroundAnimations/eep/EepAnimatedBackground";
+import GalleryAnimatedBackground from "@/components/mainPage/backgroundAnimations/gallery/GalleryAnimatedBackground";
 
 export const backgroundAnimationMap: Record<
   ThemeAnimatedBackgroundKey,
@@ -50,4 +51,5 @@ export const backgroundAnimationMap: Record<
   underwater: <UnderwaterAnimatedBackground />,
   crimson: <CrimsonAnimatedBackground />,
   eep: <EepAnimatedBackground />,
+  gallery: <GalleryAnimatedBackground />,
 };
