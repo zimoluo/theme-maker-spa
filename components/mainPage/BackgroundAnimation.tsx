2d9cd66427b8a3e1f162ddaf8f0943491cb78ce3
zimoluo <zimo_luo@outlook.com>
2024-09-12 11:37:51 -0400

    boom

--- components/mainPage/BackgroundAnimation.tsx
+++ components/mainPage/BackgroundAnimation.tsx
@@ -1,18 +1,25 @@
 "use client";
 
+import { Fragment } from "react";
 import { useSettings } from "../contexts/SettingsContext";
 import { useTheme } from "../contexts/ThemeContext";
-import { backgroundAnimationMap } from "../theme/util/backgroundAnimationMap";
+import { backgroundAnimationMap } from "@/components/theme/util/backgroundAnimationMap";
 
 export default function BackgroundAnimation() {
   const { settings } = useSettings();
   const { themeConfig } = useTheme();
 
-  const animatedBackground = themeConfig.animatedBackgroundKey;
+  const rawKey = themeConfig.animatedBackgroundKey ?? [];
+
+  const animatedBackground = Array.isArray(rawKey) ? rawKey : [rawKey];
 
   return (
     settings.backgroundRichness !== "minimal" &&
     animatedBackground &&
-    backgroundAnimationMap[animatedBackground]
+    animatedBackground.map((backgroundKey, index) => (
+      <Fragment key={`background-${index}`}>
+        {backgroundAnimationMap[backgroundKey]}
+      </Fragment>
+    ))
   );
 }
